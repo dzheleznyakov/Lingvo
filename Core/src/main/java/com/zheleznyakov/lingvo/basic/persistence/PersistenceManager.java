7e3a904e5a155fe20c5ba2194f3e3730b34c5cd1
Dmitriy Zheleznyakov <d.zheleznyakov@ocado.com>
2018-03-07 12:43:52 +0000

    Remove persistence classes from core

--- Core/src/main/java/com/zheleznyakov/lingvo/basic/persistence/PersistenceManager.java
+++
@@ -1,5 +0,0 @@
-package com.zheleznyakov.lingvo.basic.persistence;
-
-public interface PersistenceManager {
-    void persist(PersistenceEntity entity);
-}
--- Core/src/main/java/com/zheleznyakov/lingvo/basic/persistence/converters/Converter.java
+++
@@ -1,7 +0,0 @@
-package com.zheleznyakov.lingvo.basic.persistence.converters;
-
-import com.zheleznyakov.lingvo.basic.persistence.PersistenceEntity;
-
-public interface Converter<T> {
-    PersistenceEntity convert(T object);
-}
